5464cb49d698ca0b5918c928b32a72a7bbddc57f
Zalati <y4su.ced@gmail.com>
2019-04-17 14:56:53 +0200

    Update ConfigZalati.bat

--- ConfigZalati.bat
+++ ConfigZalati.bat
@@ -1,7 +1,7 @@
-cd /d D:\Program Files (x86)\Riot Games\Config
+cd /d C:\Riot Games\Riot Games\Config
 copy game.cfg game.bck
 copy input.ini input.bck
 copy PersistedSettings.json PersistedSettings.bck
-bitsadmin /transfer "ConfigLoL" https://raw.githubusercontent.com/Zalatis/League-of-Legends-config/master/Zalati/game.cfg "D:\Program Files (x86)\Riot Games\Config\game.cfg"
-bitsadmin /transfer "ConfigLoL" https://raw.githubusercontent.com/Zalatis/League-of-Legends-config/master/Zalati/input.ini "D:\Program Files (x86)\Riot Games\Config\input.ini"
-bitsadmin /transfer "ConfigLoL" https://raw.githubusercontent.com/Zalatis/League-of-Legends-config/master/Zalati/PersistedSettings.json "D:\Program Files (x86)\Riot Games\Config\PersistedSettings.json"
+bitsadmin /transfer "ConfigLoL" https://raw.githubusercontent.com/Zalatis/League-of-Legends-config/master/Zalati/game.cfg "C:\Riot Games\Riot Games\Config\game.cfg"
+bitsadmin /transfer "ConfigLoL" https://raw.githubusercontent.com/Zalatis/League-of-Legends-config/master/Zalati/input.ini "C:\Riot Games\Riot Games\Config\input.ini"
+bitsadmin /transfer "ConfigLoL" https://raw.githubusercontent.com/Zalatis/League-of-Legends-config/master/Zalati/PersistedSettings.json "C:\Riot Games\Riot Games\Config\PersistedSettings.json"
