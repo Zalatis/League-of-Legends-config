e497f1a47e7390760543baeb3f607b5b143d08ee
Zalati <y4su.ced@gmail.com>
2021-11-05 22:33:14 +0100

    Delete ConfigInstaller(Basic Installation) directory

--- ConfigInstaller(Basic Installation)/ConfigZalati.bat
+++
@@ -1,7 +0,0 @@
-cd /d C:\Riot Games\Riot Games\Config
-copy game.cfg game.bck
-copy input.ini input.bck
-copy PersistedSettings.json PersistedSettings.bck
-bitsadmin /transfer "ConfigLoL" https://raw.githubusercontent.com/Zalatis/League-of-Legends-config/master/Zalati/game.cfg "C:\Riot Games\League of Legends\Config\game.cfg"
-bitsadmin /transfer "ConfigLoL" https://raw.githubusercontent.com/Zalatis/League-of-Legends-config/master/Zalati/input.ini "C:\Riot Games\League of Legends\Config\input.ini"
-bitsadmin /transfer "ConfigLoL" https://raw.githubusercontent.com/Zalatis/League-of-Legends-config/master/Zalati/PersistedSettings.json "C:\Riot Games\League of Legends\Config\PersistedSettings.json"
