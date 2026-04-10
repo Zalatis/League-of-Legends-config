b155cc265cfb5a4e559831e7917e02f7232f6c23
Zalati (Cédric Lecullié) <y4su.ced@gmail.com>
2026-04-10 18:45:19 +0200

    Update ConfigZalati.bat

--- ConfigZalati.bat
+++ ConfigZalati.bat
@@ -1,4 +1,4 @@
-cd /d "C:\Riot Games\League of Legends\Config"
+cd /d "Z:\Riot Games\League of Legends\Config"
 copy game.cfg game.bck
 copy input.ini input.bck
 copy PersistedSettings.json PersistedSettings.bck
