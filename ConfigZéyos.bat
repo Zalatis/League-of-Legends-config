381e7355adb24d9e200a83a15f8b4b57cfda1393
Zalati (Cédric Lecullié) <y4su.ced@gmail.com>
2026-05-10 00:44:09 +0200

    Update ConfigZéyos.bat

--- ConfigZéyos.bat
+++ ConfigZéyos.bat
@@ -1,7 +1,7 @@
-cd /d D:\Program Files\Config
+cd /d "D:\Program Files\Config"
 copy game.cfg game.bck
 copy input.ini input.bck
 copy PersistedSettings.json PersistedSettings.bck
-%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe -command "Start-BitsTransfer -Source https://raw.githubusercontent.com/Zalatis/League-of-Legends-config/master/Z%C3%A9yos/game.cfg -Destination game.cfg"
-%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe -command "Start-BitsTransfer -Source https://raw.githubusercontent.com/Zalatis/League-of-Legends-config/master/Z%C3%A9yos/input.ini -Destination input.ini"
-%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe -command "Start-BitsTransfer -Source https://raw.githubusercontent.com/Zalatis/League-of-Legends-config/master/Z%C3%A9yos/PersistedSettings.json -Destination PersistedSettings.json"
+%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe -command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/Zalatis/League-of-Legends-config/master/Z%C3%A9yos/game.cfg' -OutFile 'game.cfg'"
+%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe -command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/Zalatis/League-of-Legends-config/master/Z%C3%A9yos/input.ini' -OutFile 'input.ini'"
+%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe -command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/Zalatis/League-of-Legends-config/master/Z%C3%A9yos/PersistedSettings.json' -OutFile 'PersistedSettings.json'"
