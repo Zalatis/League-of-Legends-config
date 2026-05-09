59da3ba4968688fa089d4b9fee27edac147482d4
Zalati (Cédric Lecullié) <y4su.ced@gmail.com>
2026-05-10 00:48:55 +0200

    Update download URLs in ConfigZalati.bat

--- ConfigZalati.bat
+++ ConfigZalati.bat
@@ -2,6 +2,6 @@ cd /d "Z:\Riot Games\League of Legends\Config"
 copy game.cfg game.bck
 copy input.ini input.bck
 copy PersistedSettings.json PersistedSettings.bck
-%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe -command "Invoke-WebRequest -Uri 'https://zalati.fr/download/LoL/config/Zalati/game.cfg' -OutFile 'game.cfg'"
-%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe -command "Invoke-WebRequest -Uri 'https://zalati.fr/download/LoL/config/Zalati/input.ini' -OutFile 'input.ini'"
-%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe -command "Invoke-WebRequest -Uri 'https://zalati.fr/download/LoL/config/Zalati/PersistedSettings.json' -OutFile 'PersistedSettings.json'"
+%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe -command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/Zalatis/League-of-Legends-config/master/Zalati/game.cfg' -OutFile 'game.cfg'"
+%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe -command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/Zalatis/League-of-Legends-config/master/Zalati/input.ini' -OutFile 'input.ini'"
+%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe -command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/Zalatis/League-of-Legends-config/master/Zalati/PersistedSettings.json' -OutFile 'PersistedSettings.json'"
